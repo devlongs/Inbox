625a5ad3e6f68785afe11b655829ac68b5f63a8d
Longs Pemun Gotar <developerlongs@gmail.com>
2022-07-21 21:55:41 +0000

    added nice comments to Inbox.sol

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.17; // I intentionally used older version of soliidity 
 
 contract Inbox {
     string public message;
     
+    // The function below is the same as the constructor method in newer versions of solidity
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
