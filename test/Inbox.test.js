1696322ff3a9e935b3dfbfa5971acaa875bcb9aa
Longs Pemun Gotar <developerlongs@gmail.com>
2022-07-22 08:43:41 +0000

    added more testing code

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
 /* An example of how testing works with mocha */
 // class Car {
@@ -27,3 +28,23 @@ const web3 = new Web3(ganache.provider());
 //     assert.equal(car.drive(), "vroom");
 //   });
 // });
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
